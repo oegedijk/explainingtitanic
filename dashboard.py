3481a482473a9457bb6a1dd4a1ba3cbd906b796a
Oege Dijk <oegedijk@gmail.com>
2020-06-03 22:24:10 +0200

    updated to Explainer nomenclature and decision_trees instead of shadow_trees

--- dashboard.py
+++ dashboard.py
@@ -17,7 +17,7 @@ model = RandomForestClassifier(n_estimators=50, max_depth=5)
 model.fit(X_train, y_train)
 
 print('building ExplainerBunch...')
-explainer = RandomForestClassifierBunch(model, X_test, y_test, roc_auc_score, 
+explainer = RandomForestClassifierExplainer(model, X_test, y_test, roc_auc_score, 
                                cats=['Sex', 'Deck', 'Embarked'],
                                idxs=test_names, 
                                labels=['Not survived', 'Survived'])
@@ -28,7 +28,7 @@ db = ExplainerDashboard(explainer,
                         contributions=True,
                         shap_dependence=True,
                         shap_interaction=True,
-                        shadow_trees=True)
+                        decision_trees=True)
 
 server = db.app.server
 
