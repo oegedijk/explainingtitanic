4efc330cc34cc6751f22272a0016f86b6741b396
Oege Dijk <oegedijk@gmail.com>
2020-12-11 11:14:19 +0100

    some edits to custom layout

--- index_layout.py
+++ index_layout.py
@@ -211,23 +211,23 @@ custom_card = dbc.Card(
 from explainerdashboard import ExplainerDashboard
 from explainerdashboard.custom import *
 
-import dash_bootstrap_components as dbc
-import dash_html_components as html
 
 class CustomModelTab(ExplainerComponent):
     def __init__(self, explainer):
-        super().__init__(explainer, title="Titanic Explainer")
+        super().__init__(explainer, title="Model Summary")
         self.precision = PrecisionComponent(explainer,
-                                hide_cutoff=True, hide_binsize=True,
-                                hide_binmethod=True, hide_multiclass=True,
+                                title='Precision',
+                                hide_subtitle=True, hide_footer=True,
                                 hide_selector=True,
                                 cutoff=None)
         self.shap_summary = ShapSummaryComponent(explainer,
-                                hide_title=True, hide_selector=True,
+                                title='Impact',
+                                hide_subtitle=True, hide_selector=True,
                                 hide_depth=True, depth=8,
                                 hide_cats=True, cats=True)
         self.shap_dependence = ShapDependenceComponent(explainer,
-                                hide_title=True, hide_selector=True,
+                                title='Dependence',
+                                hide_subtitle=True, hide_selector=True,
                                 hide_cats=True, cats=True,
                                 hide_index=True,
                                 col='Fare', color_col="PassengerClass")
@@ -240,57 +240,53 @@ class CustomModelTab(ExplainerComponent):
         return dbc.Container([
             dbc.Row([
                 dbc.Col([
-                    html.H3("Model Performance"),
+                     html.H3("Model Performance"),
                     html.Div("As you can see on the right, the model performs quite well."),
-                    html.Div("The higher the predicted probability of survival predicted by"
+                    html.Div("The higher the predicted probability of survival predicted by "
                             "the model on the basis of learning from examples in the training set"
-                            ", the higher is the actual percentage for a person surviving in "
+                            ", the higher is the actual percentage of passengers surviving in "
                             "the test set"),
-                ], width=4),
+                ], width=4, style=dict(margin=30)),
                 dbc.Col([
-                    html.H3("Model Precision Plot"),
                     self.precision.layout()
-                ])
+                ], style=dict(margin=30))
             ]),
             dbc.Row([
                 dbc.Col([
-                    html.H3("Feature Importances Plot"),
                     self.shap_summary.layout()
-                ]),
+                ], style=dict(margin=30)),
                 dbc.Col([
-                    html.H3("Feature importances"),
+                    html.H3("Feature Importances"),
                     html.Div("On the left you can check out for yourself which parameters were the most important."),
-                    html.Div(f"{self.explainer.columns_ranked_by_shap(cats=True)[0]} was the most important"
+                    html.Div(f"Clearly {self.explainer.columns_ranked_by_shap(cats=True)[0]} was the most important"
                             f", followed by {self.explainer.columns_ranked_by_shap(cats=True)[1]}"
                             f" and {self.explainer.columns_ranked_by_shap(cats=True)[2]}."),
                     html.Div("If you select 'detailed' you can see the impact of that variable on "
                             "each individual prediction. With 'aggregate' you see the average impact size "
-                            "of that variable on the finale prediction."),
+                            "of that variable on the final prediction."),
                     html.Div("With the detailed view you can clearly see that the the large impact from Sex "
                             "stems both from males having a much lower chance of survival and females a much "
                             "higher chance.")
-                ], width=4)
+                ], width=4, style=dict(margin=30)),
             ]),
             dbc.Row([
                 dbc.Col([
-                    html.H3("Relations between features and model output"),
-                    html.Div("In the plot to the right you can see that the higher the priace"
-                            "of the Fare that people paid, the higher the chance of survival. "
+                    html.H3("Feature dependence"),
+                    html.Div("In the plot to the right you can see that the higher the cost "
+                            "of the fare that passengers paid, the higher the chance of survival. "
                             "Probably the people with more expensive tickets were in higher up cabins, "
                             "and were more likely to make it to a lifeboat."),
-                    html.Div("When you color the impacts by the PassengerClass, you can clearly see that "
+                    html.Div("When you color the impacts by PassengerClass, you can clearly see that "
                             "the more expensive tickets were mostly 1st class, and the cheaper tickets "
                             "mostly 3rd class."),
-                    html.Div("On the right you can check out for yourself how different features impact "
+                    html.Div("On the right you can check out for yourself how different features impacted "
                             "the model output."),
-                ], width=4),
+                ], width=4, style=dict(margin=30)),
                 dbc.Col([
-                    html.H3("Feature impact plot"),
                     self.shap_dependence.layout()
-                ]),
+                ], style=dict(margin=30)),
             ])
         ])
-
 
 class CustomPredictionsTab(ExplainerComponent):
     def __init__(self, explainer):
@@ -341,8 +337,6 @@ class CustomPredictionsTab(ExplainerComponent):
                 ]),
             ])
         ])
-
-
 
 ExplainerDashboard(explainer, [CustomModelTab, CustomPredictionsTab], 
                         title='Titanic Explainer', header_hide_selector=True,
@@ -397,6 +391,12 @@ You can install the library with:
 
 ```
     pip install explainerdashboard
+```
+
+or:
+
+```
+    conda install -c conda-forge explainerdashboard
 ```
 
 """)
